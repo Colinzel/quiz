16415f349d615dd51a169bbb0a133a62a9bc46c5
Colin Zelin <Colin@Colin-Zelins-MacBook-Pro.local>
2014-01-15 10:03:11 -0500

    Initial Commit

---
+++ quiz/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  quiz
+//
+//  Created by Colin Zelin on 1/15/14.
+//  Copyright (c) 2014 Colin Zelin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
---
+++ quiz/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  quiz
+//
+//  Created by Colin Zelin on 1/15/14.
+//  Copyright (c) 2014 Colin Zelin. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
